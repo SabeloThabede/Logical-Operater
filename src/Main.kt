a84b3a7add16d6cf6be02c93499b9ecbfb915388
Sabelo Thabede <sabelothabede01@gmail.com>
2025-02-21 14:31:00 +0200

    Commit Changes

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,28 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-   
+
+    val isUserLoggedIn = true
+    val hasPremiumAccess = false
+
+    // User can access premium content only if logged in AND has premium access
+    if (isUserLoggedIn && hasPremiumAccess) {
+        println("Welcome to Premium Content!")
+    } else {
+        println("Upgrade to Premium to Access this feature.")
+    }
+
+    // User can access free content if logged in OR has a guest account
+    val isGuestUser = true
+    if (isUserLoggedIn || isGuestUser) {
+        println("You can access free content.")
+    }
+
+    // Logout scenario using NOT (!)
+    if (!isUserLoggedIn) {
+        println("Please log in first.")
+    }
+
+
+
 }
